fc13b71f4d8c2c5ca04396d2e0f1645e861e2ef2
DESKTOP-FLF0E7C\herna <navarro.hernan97@gmail.com>
2023-02-06 23:43:03 -0300

    Add redis and fix methods

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
 const port = 3000;
+const redisClient = redis.createClient(6379);
+
+// redis init
+(async () => {
+    await redisClient.connect();
+})();
+
+console.log("Connecting to the Redis");
+
+redisClient.on("ready", () => {
+    console.log("Connected!");
+});
+
+redisClient.on("error", (err) => {
+    console.log("Error in the Connection");
+});
+// redis end
 
 app.use(express.json());
 
@@ -16,7 +34,7 @@ app.get('/add/:a/:b', (req, res) => {
 app.get('/subtract/:a/:b', (req, res) => {
     const valorA = parseFloat(req.params.a);
     const valorB = parseFloat(req.params.b);
-    count+= valorA-valorB;
+    count = count-valorA-valorB;
     res.send({'value': count})
 })
 
